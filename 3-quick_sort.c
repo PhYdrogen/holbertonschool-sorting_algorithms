1b112eea3025a66d84cc5d86ac97cf98be23913a
Gabriel <fossecruor@gmail.com>
2023-01-04 05:46:41 -0800

    Pushing task3

---
+++ 3-quick_sort.c
@@ -0,0 +1,86 @@
+#include "sort.h"
+/**
+ * partition - swap and mix
+ * @array: the array
+ * @bas: lower value
+ * @haut: higher value
+ * @size: the size for print
+ * Return: the pivot
+ */
+int partition(int *array, int bas, int haut, int size)
+{
+	int tmp = 0, i = 0, j = 0, change = 0, printed = 0;
+	int pivot = array[haut];
+
+	i = bas - 1;
+	if (i == 0)
+	{
+		print_array(array, size);
+		printed = 1;
+	}
+	for (j = bas; j <= (haut - 1); j++)
+	{
+		if (array[j] <= pivot)
+		{
+			//trop de ligne print_array(array, size);
+			i++;
+			/* swap */
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			/* */
+			change++;
+			//print_array(array, size);
+		}
+	}
+	if (change != 0 && printed == 0)
+		print_array(array, size);
+	i++;
+	tmp = array[i];
+	array[i] = array[haut];
+	array[haut] = tmp;
+	//print_array(array, size);
+
+	return (i);
+}
+/**
+ * quick_sort_xd - to call my recursive fnc
+ * @array: the array
+ * @low: lower value
+ * @high: higher value
+ * @size: the size for print
+ */
+void quick_sort_xd(int *array, int low, int high, int size)
+{
+	int pivot = 0;
+
+	if (low > high)
+	{
+		//print_array(array, size);
+		return;
+	}
+
+	pivot = partition(array, low, high, size);
+	//bof print_array(array, size);
+	quick_sort_xd(array, low, pivot - 1, size);
+	quick_sort_xd(array, pivot + 1, high, size);
+	//no print_array(array, size);
+
+}
+/**
+ * quick_sort - to call my recursive fnc
+ * @array: the array
+ * @size: the size for print
+ */
+void quick_sort(int *array, size_t size)
+{
+	int low = 0;
+	int high = size - 1;
+
+	if (array == NULL || size < 1)
+		return;
+
+	// non y'a pas le switch print_array(array, size);
+	quick_sort_xd(array, low, high, size);
+	print_array(array, size);
+}
